7d33696e48009b04a6116010be4140383dc556fe
Car0lin04 <carolin.fuchs.04@gmail.com>
2024-04-24 18:29:04 +0200

    imageButton Januar an MonthOverviewActivity verbunden

--- app/src/main/java/com/example/budgetplaner_30/MonthOverviewActivity.kt
+++ app/src/main/java/com/example/budgetplaner_30/MonthOverviewActivity.kt
@@ -1,6 +1,18 @@
 package com.example.budgetplaner_30
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.budgetplaner_30.databinding.ActivityIntroBinding
+import com.example.budgetplaner_30.databinding.ActivityMonthOverviewBinding
 
 class MonthOverviewActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMonthOverviewBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMonthOverviewBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
 }
--- app/src/main/java/com/example/budgetplaner_30/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/budgetplaner_30/ui/home/HomeFragment.kt
@@ -1,4 +1,5 @@
 package com.example.budgetplaner_30.ui.home
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -6,11 +7,10 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.budgetplaner_30.MonthOverviewActivity
 import com.example.budgetplaner_30.databinding.FragmentHomeBinding
-class HomeFragment : Fragment() {
+/*class HomeFragment : Fragment() {
     private var _binding: FragmentHomeBinding? = null
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,5 +28,41 @@ class HomeFragment : Fragment() {
         super.onDestroyView()
         _binding = null
     }
+}*/
+class HomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentHomeBinding
+    private lateinit var viewModel: HomeViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        viewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
+
+        //binding.lifecycleOwner = this
+        //binding.lifecycleOwner = viewLifecycleOwner
+
+
+        // ImageButton Click Event zuweisen
+        binding.januaryImage.setOnClickListener {
+            navigateToMonthOverviewActivity()
+        }
+
+        return binding.root
+    }
+
+    private fun navigateToMonthOverviewActivity() {
+        val intent = Intent(activity, MonthOverviewActivity::class.java)
+        startActivity(intent)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        // Wenn Sie eine nullable Binding-Variable verwenden
+        //binding = null
+    }
 }
+
  
